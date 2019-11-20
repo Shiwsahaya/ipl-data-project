1a78cbb06c3c29398304a10c2206e33b7c69d9f1
Shiwsahaya <shiw11@yahoo.com>
2019-11-20 17:20:20 +0530

    done third querey extra run

--- src/main/java/Print.java
+++ src/main/java/Print.java
@@ -4,6 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import  java.util.*;
 public class Print {
+
+
     public  static  void noOfMatches(ArrayList<String >list)
     {
         int size=list.size();
@@ -15,82 +17,113 @@ public class Print {
         }
         System.out.println("1. No. of matched played per year:" +count);
     }
+
+
     public static void noOfWonMatch(ArrayList<String>list)
     {
-       // System.out.println(list);
-        int kxip=0,rcb=0,mi=0,kkr=0,srh=0,gl=0,dd=0,rps=0,csk=0,rr=0,other=0,dc=0,pw=0,ktk=0;
         int size=list.size();
+        HashMap<String,Integer>map=new HashMap<>();
         for(int i=0;i<size;i++)
         {
-            if(list.get(i).contains("Punjab")||list.get(i).contains("punjab"))
-                kxip++;
-            else if(list.get(i).contains("Bangalore"))
-                rcb++;
-            else if(list.get(i).equals("Mumbai Indians"))
-                mi++;
-            else if(list.get(i).equals("Kolkata Knight Riders"))
-                kkr++;
-            else if(list.get(i).equals("Sunrisers Hyderabad"))
-                srh++;
-            else if(list.get(i).equals("Gujarat Lions"))
-                gl++;
-            else if(list.get(i).equals("Delhi Daredevils"))
-                dd++;
-            else if(list.get(i).contains("Pune"))
-                rps++;
-            else if(list.get(i).equals("Chennai Super Kings"))
-                csk++;
-            else if(list.get(i).equals("Rajasthan Royals"))
-                rr++;
-            else if(list.get(i).equals("Deccan Chargers"))
-                dc++;
-            else if(list.get(i).equals("Kochi Tuskers Kerala"))
-                ktk++;
-            else if(list.get(i).equals(""))   //if any column not contain any value
-               ;
-            else
-            {
-                System.out.print(": other: "+list.get(i));
-                other++;
-            }
 
+            if((list.get(i)).equals(""))
+                continue;
+                if(!map.containsKey(list.get(i)))
+                {
+                    map.put(list.get(i),1);
+                }
+                else
+                {
+                    int tmp=map.get(list.get(i));
+                    tmp=tmp+1;
+                    map.put(list.get(i),tmp);
+                }
 
         }
-        System.out.println("2.Number of matches won of all teams over all the years of IPL");
-        System.out.println("Rising Pune Supergiant: "+rps);
-        System.out.println("Kings XI Punjab: "+kxip);
-        System.out.println("Sunrisers Hyderabad: "+srh);
-        System.out.println("Mumbai Indians: "+mi);
-        System.out.println("Kolkata Knight Riders: "+kkr);
-        System.out.println("Gujarat Lions: "+gl);
-        System.out.println("Delhi Daredevils: "+dd);
-        System.out.println("Royal Challengers Bangalore"+rcb);
-        System.out.println("Chennai Super Kings"+csk);
-        System.out.println("Deccan Chargers: "+dc);
-        System.out.println("Rajasthan Royals: "+rr);
-        System.out.println("Kochi Tuskers Kerala: "+ktk);
-        System.out.println("No. of team left (if any team left then other count increment): "+other);
+        for (Map.Entry mapElement : map.entrySet()) {
+            String key = (String)mapElement.getKey();
+            int value = ((int)mapElement.getValue());
+            System.out.println(key + " : " + value);
+        }
+    }
+
+
+    public  static void noOfExtraRun(ArrayList<String>list) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader("fileCsv/deliveries.csv"));
+        String line = br.readLine();
+        HashMap<String,Integer>map=new HashMap<>();
+        while ((line = br.readLine()) != null && !line.isEmpty())
+        {
+            String[]fields= line.split(",");
+            String id=fields[0];
+            if(list.contains(id))
+            {
+
+                if(!map.containsKey(fields[2]))
+                {
+                    int tmp=Integer.parseInt(fields[16]);
+                    map.put(fields[2],tmp);
+                }
+                else
+                {
+                    int value=map.get(fields[2]);
+                    int tmp=Integer.parseInt((fields[16]));
+                    tmp=tmp+value;
+                    map.put(fields[2],tmp);
+                }
+            }
+
+        }
+      //  System.out.println(map);
+        for (Map.Entry mapElement : map.entrySet()) {
+            String key = (String)mapElement.getKey();
+            int value = ((int)mapElement.getValue());
+            System.out.println(key + " : " + value);
+        }
 
     }
     public static  void main(String args[]) throws IOException {
         BufferedReader br1 = new BufferedReader(new FileReader("fileCsv/matches.csv"));
-        ArrayList<String>idList=new ArrayList<>();
         ArrayList<String>team1List=new ArrayList<>();
         ArrayList<String>team2List=new ArrayList<>();
         ArrayList<String>wonList=new ArrayList<>();
+        ArrayList<String>session=new ArrayList<>();
+        //for third query
+        ArrayList<String>id2016=new ArrayList<>();
+        HashMap<String,String>map2=new HashMap<>();
+        //third query end
         String line1 = br1.readLine();
         while ((line1 = br1.readLine()) != null && !line1.isEmpty()) {
             String[]fields= line1.split(",");
             String id=fields[0];
-            String won=fields[10];
+            String ses=fields[1];
             String team1=fields[4];
-            idList.add(id);
+            String won=fields[10];
+            String str1=fields[1];
+            if(str1.equals("2016"))
+            {
+                id2016.add(id);
+            }
+
             wonList.add(won);
             team1List.add(team1);
+            session.add(ses);
         }
-        //System.out.println(team1List);
+       // System.out.println(id2016);
+        System.out.println("1st Query");
         noOfMatches(team1List);
+        System.out.println();
+
+
+        System.out.println("2nd Query");
         noOfWonMatch(wonList);
+        System.out.println();
+
+        System.out.println("3rd Query:Extra run");
+        noOfExtraRun(id2016);
+        System.out.println();
+
+        
 
 
     }
