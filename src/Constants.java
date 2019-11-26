e267d73bfa2d892532e39839a9c6aef76c2f959a
Shiwsahaya <Shiw11@yahoo.com>
2019-11-26 17:47:25 +0530

    IPL Project Using SQL Query

--- src/Constants.java
+++ src/Constants.java
@@ -1,14 +1,14 @@
-public final class Constants {
-    public static final String NO_OF_MATCHES="select season, count(m_id) as total_match_played from matches group by season order by season ASC";
-    public static final  String NO_OF_WON_MATCHES="select winner, count(m_id) as total_wins from matches where winner is not null group by winner";
-    public static  final  String EXTRA_RUN="select bowling_team,sum(cast(extra_runs as int)) as Total_extra_runs from deliveries where id in(select m_id from matches where season=?) group by bowling_team";
-    public static final String TOP_BOWLER="select bowler, cast(sum(cast(total_runs as float))/((count(cast(ball as float)))/6.0)as decimal(10,2)) as economy from deliveries where id in(select m_id from matches where season=?) group by bowler order by economy limit ?";
-    public static  final String URL="jdbc:postgresql://localhost:5432/postgres";
-    public static  final  String USER="postgres";
-    public static final  String PASSWORD="2214@11";
-    public static final  String YEAR_16="2016";
-    public static final  String YEAR_15="2015";
-    public static final int limit=10;
+     final class Constants {
+     static final String NO_OF_MATCHES="select season, count(m_id) as total_match_played from matches group by season order by season ASC";
+     static final  String NO_OF_WON_MATCHES="select winner, count(m_id) as total_wins from matches where winner is not null group by winner";
+     static  final  String EXTRA_RUN="select bowling_team,sum(cast(extra_runs as int)) as Total_extra_runs from deliveries where id in(select m_id from matches where season=?) group by bowling_team";
+     static final String TOP_BOWLER="select bowler, cast(sum(cast(total_runs as float))/((count(cast(ball as float)))/6.0)as decimal(10,2)) as economy from deliveries where id in(select m_id from matches where season=?) group by bowler order by economy limit 10";
+     static  final String URL="jdbc:postgresql://localhost:5432/postgres";
+     static  final  String USER="postgres";
+     static final  String PASSWORD="2214@11";
+     static final  String YEAR_16="2016";
+     static final  String YEAR_15="2015";
+     static final int limit=10;
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,51 @@
 import java.sql.*;
-import static java.lang.System.out;
+import java.util.LinkedHashMap;
+import java.util.Map;
 public class Main {
-    private static void queryResult(ResultSet rs) throws SQLException {
-        while(rs.next()) {
-            out.println(rs.getString(1) + "  " + rs.getString(2));
+    private static void printResult(LinkedHashMap<?, ?> data)
+    {
+        for (Map.Entry mapElement : data.entrySet()) {
+            System.out.println(mapElement.getKey()+"\t"+ mapElement.getValue());
         }
+        System.out.println("*****************************************************************************");
     }
-    public static void main(String[] arsg) throws SQLException {
-        Con ob=new Con();
-        Connection con=ob.createConnection();
-        PreparedStatement noOfMatchPS=con.prepareStatement(Constants.NO_OF_MATCHES);
-        ResultSet noOfMatchRS=noOfMatchPS.executeQuery();
-        queryResult(noOfMatchRS);
-        out.println("*******************************************************");
+   private static LinkedHashMap<String ,String>parseLine(String sql) throws SQLException {
+       String str="";
+       return parseLine(sql,str);
+   }
+   private static LinkedHashMap<String,String>parseLine( String sql, String change) throws SQLException {
+       Con ob=new Con();
+       Connection con=ob.createConnection();
+       LinkedHashMap<String ,String>queryResult=new LinkedHashMap<>();
+       PreparedStatement preparedStatement=con.prepareStatement(sql);
+       ResultSet resultSet;
+      if(change.equals(""))
+      {
+             resultSet=preparedStatement.executeQuery();
+      }
+      else
+      {
+          preparedStatement.setString(1,change);
+          resultSet=preparedStatement.executeQuery();
+      }
+       while (resultSet.next())
+       {
+           queryResult.put(resultSet.getString(1),resultSet.getString(2));
+       }
+      return queryResult;
+   }
+    public static void main(String[] args) throws SQLException {
+        LinkedHashMap<String,String> mapForResult;
+        mapForResult=parseLine(Constants.NO_OF_MATCHES);
+        printResult(mapForResult);
 
-        PreparedStatement noOfWonMatchPS=con.prepareStatement(Constants.NO_OF_WON_MATCHES);
-        ResultSet noOfWonMatchRS=noOfWonMatchPS.executeQuery();
-        queryResult(noOfWonMatchRS);
-        out.println("*******************************************************");
+        mapForResult=parseLine(Constants.NO_OF_WON_MATCHES);
+        printResult(mapForResult);
 
-        PreparedStatement extraRunPS=con.prepareStatement(Constants.EXTRA_RUN);
-        extraRunPS.setString(1,Constants.YEAR_16);
-        ResultSet extraRunRS=extraRunPS.executeQuery();
-        queryResult(extraRunRS);
-        out.println("*******************************************************");
+        mapForResult=parseLine(Constants.EXTRA_RUN,Constants.YEAR_16);
+        printResult(mapForResult);
 
-        PreparedStatement bestBowlerPS=con.prepareStatement(Constants.TOP_BOWLER);
-        bestBowlerPS.setString(1,Constants.YEAR_15);
-        bestBowlerPS.setInt(2,Constants.limit);
-        ResultSet bestBowlerRS=bestBowlerPS.executeQuery();
-        queryResult(bestBowlerRS);
-        out.println("*******************************************************");
+        mapForResult=parseLine(Constants.TOP_BOWLER,Constants.YEAR_15);
+        printResult(mapForResult);
     }
 }
